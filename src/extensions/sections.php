625489eadf30ee5d38869e2a39d4f0d2edd89246
Johann Schopplich <mail@johannschopplich.com>
2024-01-25 10:44:38 +0100

    fix: always lowercase content field keys

--- src/extensions/sections.php
+++ src/extensions/sections.php
@@ -7,9 +7,38 @@ return [
         'props' => [
             'label' => fn ($label = null) => I18n::translate($label, $label),
             'confirm' => fn ($confirm = true) => $confirm,
-            'syncableFields' => fn ($syncableFields = null) => $syncableFields,
-            'translatableFields' => fn ($translatableFields = null) => $translatableFields,
-            'translatableBlocks' => fn ($translatableBlocks = null) => $translatableBlocks
+            'syncableFields' => function ($syncableFields = null) {
+                if (is_array($syncableFields)) {
+                    return array_map('strtolower', $syncableFields);
+                }
+
+                return $syncableFields;
+            },
+            'translatableFields' => function ($translatableFields = null) {
+                if (is_array($translatableFields)) {
+                    return array_map('strtolower', $translatableFields);
+                }
+
+                return $translatableFields;
+            },
+            'translatableStructureFields' => function ($translatableStructureFields = null) {
+                if (is_array($translatableStructureFields)) {
+                    return array_map('strtolower', $translatableStructureFields);
+                }
+
+                return $translatableStructureFields;
+            },
+            'translatableBlocks' => function ($translatableBlocks = null) {
+                if (is_array($translatableBlocks)) {
+                    $translatableBlocks = array_change_key_case($translatableBlocks, CASE_LOWER);
+                    foreach ($translatableBlocks as $block => $fields) {
+                        if (!is_array($fields)) $fields = [$fields];
+                        $translatableBlocks[$block] = array_map('strtolower', $fields);
+                    }
+                }
+
+                return $translatableBlocks;
+            }
         ],
         'computed' => [
             'config' => function () {
