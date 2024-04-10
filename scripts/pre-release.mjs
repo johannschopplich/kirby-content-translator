c4daaac07ec3a78691d7ab16b60ef206fb03d27e
Johann Schopplich <mail@johannschopplich.com>
2024-04-10 14:33:25 +0200

    chore: pre-release script to update Composer version

---
+++ scripts/pre-release.mjs
@@ -0,0 +1,6 @@
+import { fs } from "zx";
+
+const { version } = await fs.readJson("./package.json");
+const composer = await fs.readJson("./composer.json");
+composer.version = version;
+await fs.writeJson("./composer.json", composer, { spaces: 2 });
